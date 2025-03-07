904bfe185d872efbd591e102f40aa7e1a13a7fdb
류연경 <ykr.dev@gmail.com>
2025-03-07 15:10:08 +0900

    HTTP Basic 인증 공부 - AuthenticationProvider 구현 및 재정의

--- ssia-ch2-ex5/src/main/java/com/laurentiuspilca/ssia/config/ProjectConfig.java
+++ ssia-ch2-ex5/src/main/java/com/laurentiuspilca/ssia/config/ProjectConfig.java
@@ -6,13 +6,30 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
+import javax.swing.*;
+
+/*
+    WebSecurityConfigurerAdapter 상속의 문제점
+    1. configure 메소드를 오버라이딩하기 때문에 유연성이 부족하고, 필요 없는 기능까지 포함됨
+    2. 인증과 인가가 혼합 -> 관심사 분리가 어렵고, 유지보수성이 떨어짐
+    3. Spring Security는 Filter 기반으로 동작하지만, WebSecurityConfigurerAdapter는 매서드 오버라이딩 방식으로
+    보안 설정을 강제함
+
+    Spring Security 5.7+에서 공식적으로 권장하는 방법 -> SecurityFilterChain을 빈으로 등록
+    1. 클래스를 상속받지 않고 보안 설정 가능
+    2. 인증과 인가 분리 -> 코드 가독성 증가
+    3. Spring Security의 내부 동작 방식(Filter 기반)과 자연스럽게 연결됨
+
+*/
 @Configuration
 public class ProjectConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    private CustomAuthenticationProvider authenticationProvider;
+    private CustomAuthenticationProvider authenticationProvider; // CustomAuthenticationProvider 주입
 
+    // AuthenticationManagerBuilder에서 CustomAuthenticationProvider 등록
     @Override
     protected void configure(AuthenticationManagerBuilder auth) {
         auth.authenticationProvider(authenticationProvider);
--- ssia-ch2-ex5/src/main/java/com/laurentiuspilca/ssia/security/CustomAuthenticationProvider.java
+++ ssia-ch2-ex5/src/main/java/com/laurentiuspilca/ssia/security/CustomAuthenticationProvider.java
@@ -8,15 +8,18 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
-
+// AuthenticationProvider 재정의
 @Component
 public class CustomAuthenticationProvider implements AuthenticationProvider {
 
+    //  인증 로직 구현 - 인증 요청이 들어오면 실행되는 메서드
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        String username = authentication.getName();
-        String password = String.valueOf(authentication.getCredentials());
+        String username = authentication.getName(); // 요청된 사용자의 아이디
+        String password = String.valueOf(authentication.getCredentials()); // 요청된 사용자의 비밀번호
 
+        // 위와 같이 인증 논리를 대체해 UserDetailsService, PasswordEncoder의 역할을 대체할 수도 있다.
+        // 기본 구현이 요구사항과 많이 다를 경우 맞춤형 인증 논리를 구현하기 위해 사용한다.
         if ("john".equals(username) && "12345".equals(password)) {
             return new UsernamePasswordAuthenticationToken(username, password, Arrays.asList());
         } else {
@@ -24,6 +27,7 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
         }
     }
 
+    // 지원하는 인증 타입 정의 (5장)
     @Override
     public boolean supports(Class<?> authenticationType) {
         return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authenticationType);

8af5c331d99ff902d0401b04d28bb9a78162d569
류연경 <ykr.dev@gmail.com>
2025-03-07 14:11:05 +0900

    HTTP Basic 인증 공부 - UserDetailsService 및 엔드포인트 권한 부여 재정의

--- ssia-ch2-ex1/src/main/java/com/laurentiuspilca/ssia/controllers/HelloController.java
+++ ssia-ch2-ex1/src/main/java/com/laurentiuspilca/ssia/controllers/HelloController.java
@@ -3,6 +3,7 @@ package com.laurentiuspilca.ssia.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// 기본 구성
 @RestController
 public class HelloController {
 
--- ssia-ch2-ex2/src/main/java/com/laurentiuspilca/ssia/config/ProjectConfig.java
+++ ssia-ch2-ex2/src/main/java/com/laurentiuspilca/ssia/config/ProjectConfig.java
@@ -10,12 +10,16 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+/* 기본 구성 재정의
+UserDetailService를 재정의 할 때는 PasswordEncoder도 같이 선언해야 함 */
 @Configuration
 public class ProjectConfig extends WebSecurityConfigurerAdapter {
 
+    // UserDetailService 재정의
     @Override
     @Bean
     public UserDetailsService userDetailsService() {
+        // 편의상 UserDetailsService를 직접 구현하지 않고 InMemoryUserDetailsManager 사용
         var userDetailsService = new InMemoryUserDetailsManager();
 
         var user = User.withUsername("john")
@@ -28,14 +32,16 @@ public class ProjectConfig extends WebSecurityConfigurerAdapter {
         return userDetailsService;
     }
 
+    // passwordEncoder 재정의
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
     }
 
+    // 엔드포인트 권한 부여 재정의 - WebSecurityConfigurerAdapter를 확장하여 configure 재정의
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.httpBasic();
-        http.authorizeRequests().anyRequest().authenticated();
+        http.authorizeRequests().anyRequest().authenticated(); // permitAll() : 인증 없이 요청
     }
 }
